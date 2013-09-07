bb4acfa642dea23e80ac9f85889d33cca6b7df63
Lieven Govaerts <lgo@mobsol.be>
2013-09-07 18:15:14 +0200

    * buildcov.sh: copy testcases to run the serf_response tests.

--- buildcov.sh
+++ buildcov.sh
@@ -15,6 +15,8 @@ cp $SERFSRC/build/check.py build/
 cp $SERFSRC/test/*.pem test/
 cp $SERFSRC/test/server/*.pem test/server/
 cp $SERFSRC/test/server/*.p12 test/server/
+mkdir -p test/testcases
+cp $SERFSRC/test/testcases/* test/testcases/
 
 # now run the test suite to measure test coverage
 scons -Y $SERFSRC check
